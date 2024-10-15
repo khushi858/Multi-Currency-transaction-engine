5d6665a8eaf5812fcadadce51ba93491077bd36a
Khushi Upadhyay <161485884+khushi858@users.noreply.github.com>
2024-10-15 15:38:58 +0530

    Update cryptsy-quote.go

--- cryptsy-quote.go
+++ cryptsy-quote.go
@@ -1,32 +1,66 @@
-package main
+package mucupa
 
 import (
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-type CryptsyResponse struct {
-    Success int `json:"success"`
-    Return  map[string]map[string]float64 `json:"return"`
+func CryptsyGetMarket(n string) {
+	url := "http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=" + n
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("1 ERror in CryptsyGetMarket")
+		fmt.Println(err)
+	} else {
+		fmt.Println("our request to Cryptsy:")
+		fmt.Printf("%v", req)
+		fmt.Println()
+	}
+	cli := &http.Client{}
+	resp, err := cli.Do(req)
+
+	if err != nil {
+		fmt.Println("ERror in CryptsyGetMarket")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Our response from Cryptsy:")
+		fmt.Printf("%v", resp)
+		fmt.Println()
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var j interface{}
+	fmt.Println("Body from Cryptsy:")
+	fmt.Println(body)
+
+	fmt.Println("Body from Cryptsy (cast as string):")
+	bodyString := string(body[:])
+	fmt.Println(bodyString)
+
+	err2 := json.Unmarshal(body, &j)
+
+	if err2 == nil {
+		fmt.Println("Unmarshaled JSON:")
+		fmt.Printf("%v", j)
+	}
+
+	// testing output
+	CryptsyTestLog(bodyString)
+
 }
 
-func GetExchangeRate(currency string) (float64, error) {
-    url := "https://api.cryptsy.com/api/marketdata"
+func CryptsyTestLog(textToLog string) {
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("error opening file: %v", err)
+	}
+	defer f.Close()
 
-    resp, err := http.Get(url)
-    if err != nil {
-        return 0, err
-    }
-    defer resp.Body.Close()
-
-    var result CryptsyResponse
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        return 0, err
-    }
-
-    if result.Success == 1 {
-        return result.Return[currency]["lasttradeprice"], nil
-    }
-
-    return 0, nil
+	log.SetOutput(f)
+	log.Println(textToLog)
 }
